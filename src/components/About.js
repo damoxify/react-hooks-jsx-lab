dfcda8aea32251ce6ea0b6ffca9ab56a6edb645f
Adedamola Ajasa <azeemajasa@gmail.com>
2023-08-29 22:48:00 +0100

    Completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { image } from "../data/data";
+import { image } from "../data/data"
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>My journey with Moringa Software Engineering has shaped my thinking to align with that of a developer.</p>
+      <img src={image} alt="I made this"/>
+    </div>
+    );
 }
 
 export default About;
